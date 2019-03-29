fb4ec63015bb9e673e516766b21a39beb81ab141
Mac <nmacawile@gmail.com>
2019-03-29 16:21:31 +0000

    Add JWT decoder/encoder

---
+++ lib/json_web_token.rb
@@ -0,0 +1,15 @@
+class JsonWebToken
+  HMAC_SECRET = Rails.application.credentials.secret_key_base
+  
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, HMAC_SECRET)
+  end
+  
+  def self.decode(token)
+    decoded_token = JWT.decode(token, HMAC_SECRET)[0]
+    HashWithIndifferentAccess.new(decoded_token)
+  rescue JWT::DecodeError => e
+    raise ExceptionHandler::InvalidToken, e.message
+  end
+end
