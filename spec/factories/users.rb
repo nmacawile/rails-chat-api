c79900b92dce5c25db6710f0f7c90945547ba74e
Mac <nmacawile@gmail.com>
2019-03-30 16:21:50 +0000

    Fix: make email in users factory lowercase

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { 'User@email.com' }
+    email { 'user@email.com' }
     password { 'password1234' }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
