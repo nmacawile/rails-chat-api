40b5040218c402f762248c2dddebe7236a1ca0e8
Mac <nmacawile@gmail.com>
2019-05-06 11:46:40 +0000

    Allow all action cable request origins for dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
