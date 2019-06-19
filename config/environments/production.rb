c96d05a4d580328cf260773d29b1115afb3cf9ce
Mac <nmacawile@gmail.com>
2019-06-19 07:26:21 +0000

    Add CORS and action cable whitelist for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = "wss://#{ENV['HOST']}/cable"
-  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  config.action_cable.allowed_request_origins = [ENV['FRONTEND_URL']]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins ENV['FRONTEND_URL']
 
     resource '*',
       headers: :any,
